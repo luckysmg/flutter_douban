4a4014ac1b596d6669d748b6386df88dc5285550
luckysmg <2539699336@qq.com>
2019-08-19 22:33:16 +0800

    提交

--- lib/pages/book_music_pages/kou_bei_page.dart
+++ lib/pages/book_music_pages/kou_bei_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+///
+/// @created by 文景睿
+/// description:一周口碑榜
+///
 class KouBeiPage extends StatefulWidget {
   @override
   _KouBeiPageState createState() => _KouBeiPageState();
@@ -10,6 +14,9 @@ class _KouBeiPageState extends State<KouBeiPage> {
   @override
   void initState() {
     super.initState();
+    if(context == null){
+      print('null');
+    }
   }
 
   @override
