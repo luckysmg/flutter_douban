00dbd9aa3091fabcf5b75d6355f040f82a1b139b
luckysmg <2539699336@qq.com>
2019-09-16 23:26:58 +0800

    提交

--- lib/pages/book_music_pages/kou_bei_page/view.dart
+++ lib/pages/book_music_pages/kou_bei_page/view.dart
@@ -1,14 +1,15 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_douban/widgets/common_widgets/skeleton_view.dart';
 
 import 'state.dart';
 
 Widget buildView(
     KouBeiState state, Dispatch dispatch, ViewService viewService) {
-//  if (state.data == null) {
-//    return SkeletonView();
-//  }
+  if (state.itemList == null) {
+    return SkeletonView();
+  }
 
   ListAdapter adapter = viewService.buildAdapter();
   return Scaffold(
@@ -20,10 +21,11 @@ Widget buildView(
         viewService.buildComponent('appBar'),
         viewService.buildComponent('topHeader'),
         viewService.buildComponent('persistentHeader'),
-
         SliverList(
-          delegate: SliverChildBuilderDelegate(adapter.itemBuilder,
-              childCount: adapter.itemCount),
+          delegate: SliverChildBuilderDelegate(
+            adapter.itemBuilder,
+            childCount: adapter.itemCount,
+          ),
         )
       ],
     ),
